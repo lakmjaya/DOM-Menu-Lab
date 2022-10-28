a3255be4d2315e04cdecb92656503a575a6b0545
DunstWalmart GitHub <lakmjaya@gmail.com>
2022-10-28 00:00:45 -0400

    second commit

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ var menuLinks = [
     {text: 'account', href: '/account'},
   ];
 
+  var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '#', subLinks: [
+      {text: 'all', href: '/catalog/all'},
+      {text: 'top selling', href: '/catalog/top'},
+      {text: 'search', href: '/catalog/search'},
+    ]},
+    {text: 'orders', href: '#' , subLinks: [
+      {text: 'new', href: '/orders/new'},
+      {text: 'pending', href: '/orders/pending'},
+      {text: 'history', href: '/orders/history'},
+    ]},
+    {text: 'account', href: '#', subLinks: [
+      {text: 'profile', href: '/account/profile'},
+      {text: 'sign out', href: '/account/signout'},
+    ]},
+  ];
+
   let topMenuEl = document.querySelector('#top-menu')
+  let subMenuEl = document.querySelector('#sub-menu')
+  let mainEl = document.querySelector("main")
+  let mainBG = "var(--main-bg)"
   for(let i=0; i<menuLinks.length;i++){
     let aTag = document.createElement('a')
     aTag.attributes.href = menuLinks[i].href;
@@ -14,15 +35,17 @@ var menuLinks = [
     topMenuEl.appendChild(aTag)
 }
 
-
-let mainEl = document.querySelector("main")
-mainEl.style.backgroundColor = "var(--main-bg)"
-mainEl.className='flex-ctr'
-let mainBG = "var(--main-bg)"
-console.log(mainEl)
-mainEl.innerHTML="<h1> SEI Rocks!</h1>"
-
-
 topMenuEl.style.height ="100%"
 topMenuEl.style.backgroundColor="var(--top-menu-bg)"
 topMenuEl.classList.add("flex-around")
+
+subMenuEl.style.height='100%'
+subMenuEl.style.backgroundColor='var(--sub-menu-bg)'
+subMenuEl.style.position='absolute'
+subMenuEl.style.top ='0'
+subMenuEl.classList.add('flex-around')
+
+mainEl.style.backgroundColor = "var(--main-bg)"
+mainEl.innerHTML="<h1> SEI Rocks!</h1>"
+mainEl.classList.add('flex-ctr')
+console.log(mainEl)
